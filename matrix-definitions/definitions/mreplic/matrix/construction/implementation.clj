5b2766ca6dca732039242b74d80863e5ad2d0d1d
Jasper <jasperhilven@gmail.com>
2015-01-04 14:41:30 +0100

    Updated matrix construction: finished implementation

--- matrix-definitions/definitions/mreplic/matrix/construction/implementation.clj
+++ matrix-definitions/definitions/mreplic/matrix/construction/implementation.clj
@@ -1,3 +1,24 @@
+[:create_vector
+[:VectorElementDescription
+ :i [:GetIteratorOverDimension :..] [:GetElementAt :.. :i]  ] ]
+[:create_matrix
+[:MatrixElementDescription
+ :i0 [:GetIteratorOverDimension :..] :i1 [:GetIteratorOverDimension :v]
+ [:GetElementAt [:GetElementAt :.. :i0] :i1]  ] ]
+
+
+
+[:fill_vector
+  [:VectorElementDescription
+   :i [:GetIteratorFromScalar :d]
+  :fillnumber
+  ]]
+[:fill_matrix
+  [:MatrixElementDescription  :i0 [:GetIteratorFromScalar :d0] :i1 [:GetIteratorFromScalar :d1]
+   :fillnumber]]
+[:create_unity_matrix
+  [:MatrixElementDescription :i0 [:GetIteratorFromScalar :d] :i1 [:GetIteratorFromScalar :d]
+    [:if [:= i0 i1] 1 0]]]
 [:vector_to_row_matrix
  [:MatrixElementDescription
   :d0
